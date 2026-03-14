fb75c84d23654c4d43c92ecae95800b464217272
AEndrix <redegalli.andrea03@gmail.com>
2026-03-14 02:02:05 +0100

    chore: align component scan with commons lib

--- src/main/java/it/aredegalli/coachly/user/CoachlyUsersBeApplication.java
+++ src/main/java/it/aredegalli/coachly/user/CoachlyUsersBeApplication.java
@@ -3,7 +3,7 @@ package it.aredegalli.coachly.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "it.aredegalli.coachly")
 public class CoachlyUsersBeApplication {
 
 	public static void main(String[] args) {
